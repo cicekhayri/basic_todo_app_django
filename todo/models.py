b0be5c37fabd8ddf61bc5cecec1f23f620f1129d
Hayri Cicek <hayricicek@Hayris-Air.lan>
2014-11-18 13:11:52 +0100

    indentation fix

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Todo(models.Model):
-	title = models.CharField(max_length=255)
-	description = models.TextField()
-	completed = models.BooleanField(default=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
 
-	def __unicode__(self):
-		return self.title
+    def __unicode__(self):
+        return self.title
